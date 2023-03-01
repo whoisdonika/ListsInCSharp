f721b3f09b0181173055f723d07f59f3d1a1e2ab
STUDENTS-1212-4 <donikayurukowa9@gmail.com>
2023-03-01 11:20:46 +0200

    Add project files.

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <ulong> numbers = new List<ulong>();
+            numbers.Add(1);
+            numbers.Add(1);
+
+            Console.Write("Enter count of Fibonacci numbers : ");
+            byte n = byte.Parse(Console.ReadLine());
+
+            for (int i = 2; i < n; i++)
+            {
+                numbers.Add(numbers[i - 1] + numbers[i - 2]);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+            
+        }
+    }
+}
---
+++ Zadacha1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers count : ");
+            byte n = byte.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter number : ");
+                int item = int.Parse(Console.ReadLine());
+                numbers.Add(item);
+            }
+
+            //dobavq v sredata
+            int newIndex = n / 2;
+            numbers.Insert(newIndex, 6);
+            //dobavqne na min element v kraq
+            int min = numbers.Min();
+            numbers.Add(min);
+            //iztriva vsichki nuli
+
+            while (numbers.Contains(0))
+            {
+               numbers.Remove(0);
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+
+            Console.WriteLine("Numbers count is " + numbers.Count);
+             
+        }
+    }
+}
